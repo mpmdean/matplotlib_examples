c069598b7429a22a3b642d7a9829edae44c416c8
Mark <mpmdean@gmail.com>
2017-12-24 18:10:35 +0000

    Add additional example with gradient fill

---
+++ gradient_fill.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import matplotlib.patches as patches
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFilter
+
+def zfunc(x, y, fill_color='k', alpha=1.0):
+    scale = 10
+    x = (x*scale).astype(int)
+    y = (y*scale).astype(int)
+    xmin, xmax, ymin, ymax = x.min(), x.max(), y.min(), y.max()
+
+    w, h = xmax-xmin, ymax-ymin
+    z = np.empty((h, w, 4), dtype=float)
+    rgb = mcolors.colorConverter.to_rgb(fill_color)
+    z[:,:,:3] = rgb
+
+    # Build a z-alpha array which is 1 near the line and 0 at the bottom.
+    img = Image.new('L', (w, h), 0)  
+    draw = ImageDraw.Draw(img)
+    xy = (np.column_stack([x, y]))
+    xy -= xmin, ymin
+    # Draw a blurred line using PIL
+    draw.line(map(tuple, xy.tolist()), fill=255, width=15)
+    img = img.filter(ImageFilter.GaussianBlur(radius=100))
+    # Convert the PIL image to an array
+    zalpha = np.asarray(img).astype(float) 
+    zalpha *= alpha/zalpha.max()
+    # make the alphas melt to zero at the bottom
+    n = zalpha.shape[0] // 4
+    zalpha[:n] *= np.linspace(0, 1, n)[:, None]
+    z[:,:,-1] = zalpha
+    return z
+
+def gradient_fill(x, y, fill_color='k', ax=None, zfunc=None, alpha=1, zorder=1, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+
+    if zfunc is None:
+        h, w = 100, 1
+        z = np.empty((h, w, 4), dtype=float)
+        rgb = mcolors.colorConverter.to_rgb(fill_color)
+        z[:,:,:3] = rgb
+        z[:,:,-1] = np.linspace(0, alpha, h)[:,None]
+    else:
+        z = zfunc(x, y, fill_color=fill_color, alpha=alpha)
+    xmin, xmax, ymin, ymax = x.min(), x.max(), y.min(), y.max()
+    im = ax.imshow(z, aspect='auto', extent=[xmin, xmax, ymin, ymax],
+                   origin='lower', zorder=zorder)
+
+    xy = np.column_stack([x, y])
+    xy = np.vstack([[xmin, ymin], xy, [xmax, ymin], [xmin, ymin]])
+    clip_path = patches.Polygon(xy, facecolor='none', edgecolor='none', closed=True)
+    ax.add_patch(clip_path)
+    im.set_clip_path(clip_path)
+    ax.autoscale(True)
+    return im
